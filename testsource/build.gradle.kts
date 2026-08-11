48e6331f13ebfd8b4285fffe51e70d1449681b74
danish9661 <9661346164h@gmail.com>
2026-08-11 20:27:07 +0530

    Build only separate per-ABI APKs, drop universal

--- testsource/build.gradle.kts
+++ testsource/build.gradle.kts
@@ -45,7 +45,7 @@ android {
             isEnable = true
             reset()
             include("arm64-v8a", "armeabi-v7a", "x86", "x86_64")
-            isUniversalApk = true
+            isUniversalApk = false
         }
     }
 
